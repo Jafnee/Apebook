066cf2f3f2aeea98a2feeeb626ebf0495de08994
Jafnee <jafneejesmee@gmail.com>
2015-09-17 01:22:14 +0800

    Implement URL routing

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^post/(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
 ]
-
-admin.site.site_header = "Apebook Administration"
