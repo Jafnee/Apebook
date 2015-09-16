5e135b17a88ebb26d9bddebb6ff1995a6817e68e
Jafnee <jafneejesmee@gmail.com>
2015-09-16 23:16:25 +0800

    Design admin page for blog app

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Fruit, FruitEaten
+
+class FruitEatenInline(admin.StackedInline):
+    model = FruitEaten
+    verbose_name = "Fruit eaten"
+    verbose_name_plural = "Fruit eaten"
+    extra = 1
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pub_date', 'text', 'total_calories', 'meal_size')
+    inlines = [FruitEatenInline]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Fruit)
