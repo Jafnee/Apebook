6e7595c029fa5d65039a38afbbb28ec1c9fd202f
Jafnee <jafneejesmee@gmail.com>
2015-09-17 01:54:36 +0800

    root domain will now open blog

--- src/Apebook/urls.py
+++ src/Apebook/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
+    url(r'^$', include('blog.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
